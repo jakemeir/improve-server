7d9785adb7716b777a016c8d6a9777917e22e603
jakemeir <jakemeirjacobs@gmail.com>
2024-09-26 15:47:35 +0300

    Fixed database connection issues

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express, { Request, Response } from 'express';
 import SwaggerUI from 'swagger-ui-express';
 import dotenv from 'dotenv';
+import sequelize from './util/database';
 
 const swagger  = require('../swagger.json')
 
 dotenv.config();
 const app = express();
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/swagger', SwaggerUI.serve, SwaggerUI.setup(swagger));
@@ -24,6 +25,12 @@ app.get('/users', (req: Request, res: Response) => {
     }
   res.json(data);
 });
+
+
+  sequelize.authenticate().then(t=> console.log('Connection has been established successfully.')).catch(e=>console.log(e)
+  )
+  
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- src/util/database.ts
+++ src/util/database.ts
@@ -1,10 +1,15 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 
 
  const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER_NAME, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: "mysql"
+    dialect: "mysql",
+    host: 'localhost'
+    
   });
 
   export default sequelize;
