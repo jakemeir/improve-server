79de4457cc3d03e6ac470044de015b57992924e4
jacobburshtein <168734078+jacobburshtein@users.noreply.github.com>
2024-09-26 14:22:09 +0300

    Revert "Dev"

--- src/util/types.d.ts
+++
@@ -1,9 +0,0 @@
-export interface ApiResponse {
-    isSuccessful: boolean;
-    displayMessage: string | null;
-    description: string | null;
-    exception: string | null;
-    timestamp: string | null;
-    data?: object;
-  }
-
