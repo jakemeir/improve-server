a8566823d4ec72f10042e53947b7e354f003963a
jakemeir <jakemeirjacobs@gmail.com>
2024-09-29 11:27:34 +0300

    fix

--- dist/app.js
+++ dist/app.js
@@ -6,10 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const database_1 = __importDefault(require("./util/database"));
 const swagger = require('../swagger.json');
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 app.use(express_1.default.json());
 app.use('/swagger', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger));
 app.get('/users', (req, res) => {
@@ -23,6 +24,7 @@ app.get('/users', (req, res) => {
     };
     res.json(data);
 });
+database_1.default.authenticate().then(t => console.log('Connection has been established successfully.')).catch(e => console.log(e));
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
