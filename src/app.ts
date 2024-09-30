346ff4a8ad197913ee37c0160f8954e1bf1f370e
jakemeir <jakemeirjacobs@gmail.com>
2024-09-30 12:39:52 +0300

    Error Handling

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,34 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response,NextFunction  } from 'express';
 import SwaggerUI from 'swagger-ui-express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import User from './models/user';
 import userRoutes from './routes/user'
+import { error } from 'console';
 
 const swagger  = require('../swagger.json')
 
+const port = process.env.PORT || 3000;
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/swagger', SwaggerUI.serve, SwaggerUI.setup(swagger));
-
-
-
 app.use(userRoutes);
 
 
+app.use((error:Error,req:Request,res:Response,next:NextFunction)=>{
+  const data: ApiResponse = {
+    isSuccessful: false,
+    displayMessage: 'Server error',
+    exception: "Internal server error",
+    timestamp: new Date(),
+    data: null
+  };
 
+  res.status(500).json(data)
+
+})
 
 
 mongoose
