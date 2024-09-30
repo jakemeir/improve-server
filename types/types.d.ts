e5a0c44d4311a19acb3b5792bdf9ba9b1a4481a5
jakemeir <jakemeirjacobs@gmail.com>
2024-09-30 11:39:49 +0300

    finally code

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,10 +1,9 @@
 interface ApiResponse {
     isSuccessful: boolean;
     displayMessage: string | null;
-    description: string | null;
     exception: string | null;
-    timestamp: string | null;
-    data?: object;
+    timestamp: Date | null;
+    data: object | null;
   }
 
  declare namespace NodeJS {
