7a561e535601602296f1a34f29e015b148a3edda
jakemeir <jakemeirjacobs@gmail.com>
2024-10-07 15:00:35 +0300

    Added update user functionality

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,17 +1,17 @@
 import express from "express";
-import {createUser, getUser,getUsers,deleteUser} from "../controllers/user"
+import {createUser,updateUser, getUser,getUsers,deleteUser} from "../controllers/user"
+import userValidator from "../middleware/user";
 
 const router = express.Router();
 
-router.post('/users',createUser)
+router.post('/users', userValidator,createUser)
 
 router.get('/users',getUsers)
 
+router.put('/users/:userId', userValidator,updateUser)
+
 router.get('/users/:userId',getUser)
 
-
 router.delete('/users/:userId',deleteUser)
-
-
 
 export default router;
