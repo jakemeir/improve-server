b2fae35522a8d14995e58bfe874ba46e477087fe
jakemeir <jakemeirjacobs@gmail.com>
2024-10-15 14:09:04 +0300

    Create exercise API

--- src/app.ts
+++ src/app.ts
@@ -7,16 +7,38 @@ import authRoutes from './routes/auth'
 import exerciseRoutes from './routes/exercise';
 import swagger from '../swagger.json'
 import cors from 'cors';
+import path from 'path';
+import multer, { FileFilterCallback } from "multer"
+import guard from './middleware/auth'
+
+dotenv.config();
 
 
 
 const port = process.env.PORT as string || 8080;
-dotenv.config();
 const app = express();
+const fileStorage = multer.diskStorage({
+  destination: (req,file,cb)=>{
+    cb(null,"images")
+  },
+  filename: (req,file,cb) =>{
+    cb(null,new Date().toISOString()+'-'+file.originalname)
+  }
+});
+
+const fileFilter = (req:Request,file:Express.Multer.File,cb:FileFilterCallback) => {
+  if(file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg'){
+    cb(null,true);
+  }else{
+    cb(null,false);
+  }
+};
 
 app.use(cors());
 
 app.use(express.json());
+app.post('/exercises',guard,multer({storage:fileStorage,fileFilter:fileFilter}).single('image'))
+app.use("/images",express.static(path.join(__dirname,"images")))
 app.use('/swagger', SwaggerUI.serve, SwaggerUI.setup(swagger));
 app.use("/auth",authRoutes)
 app.use("/users",userRoutes);
--- src/controllers/exercise.ts
+++ src/controllers/exercise.ts
@@ -1,17 +1,30 @@
 import { Request, Response,NextFunction } from 'express';
 import Exercise from '../models/exercise';
+import { validationResult } from 'express-validator';
+import fs from "fs"
 
 export const  createExercise= async (req:Request, res:Response, next:NextFunction)=>{
+    let imgPath =''
+    
     try {
         const { name, description, sets,times,category} = req.body;
     
-        // const result = validationResult(req);
+        const result = validationResult(req);
+
+        if(!req.file){
+            const error:CustomError = new Error('no image provided');
+            error.statusCode = 422;
+            throw error;
+        }
+
+        imgPath =  req.file.path;
     
-        // if (!result.isEmpty()) {
-        //   const error:CustomError = new Error(result.array()[0].msg);
-        //   error.statusCode = 422;
-        //   throw error;
-        // }
+        if (!result.isEmpty()) {
+          const error:CustomError = new Error(result.array()[0].msg);
+          error.statusCode = 422;
+          throw error;
+        }
+
     
     
         const newExercise = new Exercise({
@@ -19,7 +32,8 @@ export const  createExercise= async (req:Request, res:Response, next:NextFunctio
           description,
           sets,
           times,
-          category
+          category,
+          imgPath
         });
     
         await newExercise.save();
@@ -35,6 +49,10 @@ export const  createExercise= async (req:Request, res:Response, next:NextFunctio
         res.status(201).json(data);
     
       } catch (error) {
+        if(imgPath){
+            fs.unlinkSync(imgPath)
+        }
+        
         next(error);
         
       }
