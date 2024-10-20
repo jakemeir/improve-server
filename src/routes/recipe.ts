e11d802af021281d2fcdef292d1762620fa3bd55
jakemeir <jakemeirjacobs@gmail.com>
2024-10-20 11:40:53 +0300

    Delete recipe API

--- src/routes/recipe.ts
+++ src/routes/recipe.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import guard from '../middleware/auth';
-import {createRecipe} from '../controllers/recipe'
+import {createRecipe,deleteRecipe} from '../controllers/recipe'
 import recipeValidator from '../middleware/recipe';
 
 
@@ -8,6 +8,9 @@ import recipeValidator from '../middleware/recipe';
 const router = express.Router();
 
 router.post('/', guard, recipeValidator, createRecipe);
+
+
+router.delete('/:recipeId',guard,deleteRecipe)
 
 
 export default router;
