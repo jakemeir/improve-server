903b89cd7c15883426eb094154845ac670dfc90f
jakemeir <jakemeirjacobs@gmail.com>
2024-10-21 15:16:08 +0300

    .

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -51,44 +51,46 @@ export const googleLogin = async (req:Request, res:Response, next:NextFunction)=
     res.redirect(`https://accounts.google.com/o/oauth2/v2/auth?client_id=${encodeURIComponent(process.env.GOOGLE_CLIENT_ID as string)}&redirect_uri=http://localhost:8080/auth/google/callback&response_type=code&scope=${encodeURIComponent('profile email')}`);
 }
 
-export const googleCallback = async (req:Request, res:Response, next:NextFunction)=>{
-    const { code } = req.query;
+export const googleCallback = async (req: Request, res: Response, next: NextFunction) => {
+  const { code } = req.query;
 
-    try {
+  try {
+    const { data } = await axios.post('https://oauth2.googleapis.com/token', {
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      code,
+      redirect_uri: process.env.GOOGLE_REDIRECT_URI,
+      grant_type: 'authorization_code',
+    });
 
-        const { data } = await axios.post('https://oauth2.googleapis.com/token', {
-            client_id: process.env.GOOGLE_CLIENT_ID,
-            client_secret: process.env.GOOGLE_CLIENT_SECRET,
-            code,
-            redirect_uri: 'http://localhost:8080/auth/google/callback',
-            grant_type: 'authorization_code',
-          });
+    const { access_token } = data;
 
-          const { access_token} = data;
 
-          const {  data : {email}} = await axios.get('https://www.googleapis.com/oauth2/v1/userinfo', {
-            headers: { Authorization: `Bearer ${access_token}` },
-          });
+    const { data: { email } } = await axios.get('https://www.googleapis.com/oauth2/v1/userinfo', {
+      headers: { Authorization: `Bearer ${access_token}` },
+    });
 
-          const user = await User.findOne({email:email})
+    const user = await User.findOne({ email });
 
-          if(!user){
-            return res.redirect("http://localhost:3000/signup")
-          }
-
-         const token = jwt.sign({email:user.email,id:user._id.toString()},
-         process.env.PRIVATE_KEY as string,
-         {expiresIn:"1h"}
-         )
-
-        res.cookie('token', token,);
-      
-        res.redirect("http://localhost:3000/users")
-
-    } catch (error) {
-        next(error)
+    if (!user) {
+      return res.redirect("http://localhost:3000/signup");
     }
-}
+    const token = jwt.sign({ email: user.email, id: user._id.toString() },
+      process.env.PRIVATE_KEY as string,
+      { expiresIn: "1h" }
+    );
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 3600000,
+    });
+
+    res.redirect("http://localhost:3000/users");
+
+  } catch (error) {
+    next(error);
+  }
+};
 
 
 export const loginOTP = async (req:Request, res:Response, next:NextFunction)=>{
