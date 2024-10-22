9f1f3efd21a07501e94a79c2ecd6d9515b004489
jakemeir <jakemeirjacobs@gmail.com>
2024-10-22 13:40:52 +0300

    .

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,30 +1,35 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import { Request, Response,NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 dotenv.config();
 
-export default async (req:Request, res:Response, next:NextFunction)=>{
-    
-    // return next()
+export default (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.get('Authorization');
 
-    const token = req.get('Authorization') || '';
-    let decodedToken;
-    try {
-
-        decodedToken = jwt.verify(token,process.env.PRIVATE_KEY as string) as jwt.JwtPayload;
-
-        
-    } catch (error) {
+    if (!authHeader) {
+        const error = new Error('Authorization header missing') as CustomError;
+        error.statusCode = 401;
         return next(error);
     }
-    if(!decodedToken){
-        const error:CustomError = new Error("user not authenticated")
-        error.statusCode = 401
+
+    const token = authHeader
+    
+    let decodedToken;
+    try {
+        decodedToken = jwt.verify(token, process.env.PRIVATE_KEY as string) as jwt.JwtPayload;
+    } catch (error) {
+        const authError = error as CustomError;
+        authError.statusCode = 401;
+        return next(authError);
+    }
+
+    if (!decodedToken) {
+        const error = new Error('User not authenticated') as CustomError;
+        error.statusCode = 401;
         return next(error);
     }
 
     req.body.userId = decodedToken.id;
-
-   next();
-}
+    next();
+};
