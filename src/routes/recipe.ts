6d57e24a11593df0eb88f3137ab48e7de9a6caf2
jacobburshtein <168734078+jacobburshtein@users.noreply.github.com>
2024-10-31 10:29:37 +0200

    Paging

--- src/routes/recipe.ts
+++ src/routes/recipe.ts
@@ -12,9 +12,9 @@ const router = express.Router();
 router.use('/', guard, multer({ storage: fileStorage, fileFilter }).single('image'));
 router.post('/', guard, recipeValidator, createRecipe);
 router.put('/', guard, recipeValidator, UpdateRecipe);
-router.get('/',guard,getRecipes)
-router.delete('/:recipeId',guard,deleteRecipe)
-router.get('/export',guard,exportRecipe)
+router.get('/',guard, getRecipes)
+router.delete('/:recipeId',guard, deleteRecipe)
+router.get('/export',guard, exportRecipe)
 
 
 export default router;
